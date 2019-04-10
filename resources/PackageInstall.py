19ea66ad25beb9da681799cf4edb9f399ba253df
Subbaraman V <subba.venkat@gmail.com>
2019-04-10 18:18:40 -0400

    Updating packaging configuration data model

--- resources/PackageInstall.py
+++ resources/PackageInstall.py
@@ -8,6 +8,10 @@ class PackageInstall(Resource):
         pkg_dict = json.load(f)
 
     def get(self, name):
-        for distro in self.pkg_dict:
-            print(distro['Name'])
+        for k, v in self.pkg_dict.items():
+            print(k, v)
+            # print(self.pkg_dict["package"])
+            #print(self.pkg_dict["service"])
+            #print(self.pkg_dict["deploy"])
+            #print(self.pkg_dict["hosts"])
         return {"message": "Package installation..." + name}
